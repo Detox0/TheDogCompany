cdd140597077887dfe1501c656c8d66ddc844a03
Sebastián Vallejos <vallejos.sa@gmail.com>
2018-03-18 18:34:09 -0300

    Se han añadido 3 modelos al proyecto, tambien se ha ejecutado la migracion sin problemas.

--- theDogCompany/models.py
+++ theDogCompany/models.py
@@ -3,4 +3,41 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+# Modelos a utilizar dentro de la aplicacion, existen tres: persona, auto y carnet
+
+class Persona(models.Model):
+
+    nombre = models.CharField(max_length=50)
+    apellidoPaterno = models.CharField(max_length=50)
+    apellidoMaterno = models.CharField(max_length=50)
+    edad = models.IntegerField(default=0)
+
+    #Necesario para que cuando se pregunte por el objeto retorne el valor indicado
+    def __str__(self):
+        return self.nombre
+
+
+class Auto(models.Model):
+
+    modelo = models.CharField(max_length=50)
+    marca = models.CharField(max_length=50)
+    patente = models.CharField(max_length=10,blank=True)
+    anio = models.IntegerField()
+
+    #Llave foranea para indicar que el auto pertenece a una persona en especifico
+    persona = models.ForeignKey(Persona, on_delete=models.CASCADE,blank=True)
+
+    # Necesario para que cuando se pregunte por el objeto retorne el valor indicado
+    def __str__(self):
+        return self.marca
+
+class Carnet(models.Model):
+
+    fechaVencimiento = models.DateField()
+    numeroSerie = models.CharField(max_length=10)
+    activo = models.BooleanField(blank=False,null=False)
+    persona = models.OneToOneField(Persona,on_delete=models.CASCADE)
+
+    # Necesario para que cuando se pregunte por el objeto retorne el valor indicado
+    def __str__(self):
+        return self.numeroSerie
